43cff0df6fa4efef3b6a4d55737849adf32164fa
Dyego Henrique Costa Braga <dyego.hcb@gmail.com>
2023-11-19 10:22:28 -0300

    EXCEPTION TRATAMENTS

--- remove-punctuation-source-code/script/remove-punctuation.py
+++ remove-punctuation-source-code/script/remove-punctuation.py
@@ -5,15 +5,24 @@ def remover_pontuacao(texto):
     texto_sem_pontuacao = texto.translate(translator)
     return texto_sem_pontuacao
 
-nome_arquivo_entrada = './input/in.txt'
-nome_arquivo_saida = './output/out.txt'
+def processar_arquivo(input_path, output_path):
+    try:
+        with open(input_path, 'r', encoding='utf-8') as input_file:
+            texto = input_file.read()
 
-with open(nome_arquivo_entrada, 'r', encoding='utf-8') as arquivo_entrada:
-    texto = arquivo_entrada.read()
+        texto_sem_pontuacao = remover_pontuacao(texto)
 
-texto_sem_pontuacao = remover_pontuacao(texto)
+        with open(output_path, 'w', encoding='utf-8') as output_file:
+            output_file.write(texto_sem_pontuacao)
 
-with open(nome_arquivo_saida, 'w', encoding='utf-8') as arquivo_saida:
-    arquivo_saida.write(texto_sem_pontuacao)
+        print(f"Pontuação removida e texto salvo em {output_path}")
 
-print(f"Pontuação removida e texto salvo em {nome_arquivo_saida}")
+    except FileNotFoundError:
+        print(f"Arquivo {input_path} não encontrado.")
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+
+input_path = './input/in.txt'
+output_path = './output/out.txt'
+
+processar_arquivo(input_path, output_path)
