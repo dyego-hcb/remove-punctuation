7703bae053c604eaec966c72335012d9f6c1796a
Dyego Henrique Costa Braga <dyego.hcb@gmail.com>
2023-12-31 16:27:55 -0300

    Update code

--- remove-punctuation-source-code/script/remove-punctuation.py
+++ remove-punctuation-source-code/script/remove-punctuation.py
@@ -1,26 +1,29 @@
 import string
 
-def remover_pontuacao(texto):
-    translator = str.maketrans('', '', string.punctuation)
-    texto_sem_pontuacao = texto.translate(translator)
-    return texto_sem_pontuacao
-
-def processar_arquivo(input_path, output_path):
+def remove_ponctuation_in_list(input_words):
     try:
-        with open(input_path, 'r', encoding='utf-8') as input_file:
-            texto = input_file.read()
+        texto = ' '.join(input_words)
 
-        texto_sem_pontuacao = remover_pontuacao(texto)
+        texto_sem_pontuacao = remover_ponctuation(texto)
 
-        with open(output_path, 'w', encoding='utf-8') as output_file:
-            output_file.write(texto_sem_pontuacao)
+        palavras_sem_pontuacao = texto_sem_pontuacao.split()
 
-        print(f"Pontuação removida e texto salvo em {output_path}")
+        return palavras_sem_pontuacao
 
-    except FileNotFoundError:
-        print(f"Arquivo {input_path} não encontrado.")
     except Exception as e:
         print(f"Ocorreu um erro: {e}")
+        return None
+
+def remover_ponctuation(texto):
+    caracteres_especiais = ['``', "''", '`', '“', '”', '‘', '’', '-', '–', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '=', '{', '}', '[', ']', '|', '\\', ':', ';', '<', '>', ',', '.', '?', '/', '"']
+
+    for char in caracteres_especiais:
+        texto = texto.replace(char, '')
+
+    texto_sem_pontuacao = ''.join(c if c.isalnum() or c.isspace() else ' ' for c in texto)
+
+    return texto_sem_pontuacao
+
 
 input_path = './input/in.txt'
 output_path = './output/out.txt'
